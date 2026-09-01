10a92f6c9bcbad6517ec5f75b747a442e18a103f
MendozaX69 <jorgezine2@gmail.com>
2026-09-01 07:02:22 -0600

    git commit -m "Added clase1.ipynb, modified clase1.py, and added netflix_titles.csv"

--- clase1.py
+++ clase1.py
@@ -1,1 +1,10 @@
-print("Hola mundo") 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics.pairwise import cosine_similarity
+
+df = pd.read_csv("roblox_games.csv")
+
+df.head()
